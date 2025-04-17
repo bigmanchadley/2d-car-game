5bf4a4075475e8adda6520f88bb8ae206b30bf37
bigmanchadley <bigmanchadley@gmail.com>
2025-04-16 21:08:16 -0400

    added correct reversing, probably did some rotation stuff too

--- scripts/car_script.gd
+++ scripts/car_script.gd
@@ -36,7 +36,7 @@ var lateral_friction
 var is_drifting = false
 var is_accelerating = false
 
-
+var rot_dir
 var rot_momentum = 0.0
 const MAX_ROT_TORQUE = 10500 # POWER * Max turning radius degree: 300 * 35
 
@@ -54,13 +54,13 @@ func _physics_process(delta):
 	acceleration(delta)
 	steering(delta)
 	
-	queue_redraw()
+	#queue_redraw()
 
 	move_and_slide()
 
 ####################################### END MAIN ###############################################
 ####################################### DEBUGGER ###############################################
-func _draw():
+#func _draw():
 	
 # 	draw_line(Vector2.ZERO, global_transform.basis_xform_inv(steer_dir) * 50, Color(0, 1, 0), 3)
 	# #draw_line(Vector2.ZERO, turn_left * 50, Color(1, 0, 0), 2)
@@ -69,14 +69,15 @@ func _draw():
 	# draw_line(Vector2.ZERO, global_transform.basis_xform_inv(dynamic_etr_left) * 50, Color(1,0,0), 4)
 	# draw_line(Vector2.ZERO, global_transform.basis_xform_inv(dynamic_etr_right) * 50, Color(1,0,0), 4)
 	# #draw_line(Vector2.ZERO, new_etr * 50, Color(0,0,1), 4)
-	draw_line(Vector2.ZERO, global_transform.basis_xform_inv(velocity) * 2, Color(1,1,1), 5) # white
+	#draw_line(Vector2.ZERO, global_transform.basis_xform_inv(velocity) * 2, Color(1,1,1), 5) # white
 	# #draw_line(Vector2.ZERO, global_transform.basis_xform_inv(accel_dir) * 50, Color(1,0,1), 4) # pink
 	# draw_line(Vector2.ZERO, global_transform.basis_xform_inv(force_addition) * 2, Color(0,0,1), 3) #blue
 	# #draw_line(Vector2.ZERO, global_transform.basis_xform_inv(maintained_vel) * 2, Color(1,1,0), 4) #yellow
 	# #draw_line(Vector2.ZERO, global_transform.basis_xform_inv(transferred_vel) * 2, Color(0,1,1), 3) #teal
 	# #draw_line(Vector2.ZERO, global_transform.basis_xform_inv(perp_vector) * 52, Color(0,0,1), 3) #blue
-	draw_line(Vector2.ZERO, global_transform.basis_xform_inv(medial_vel) * 2, Color(0.5,0.5,1), 3) #light blue
-	draw_line(Vector2.ZERO, global_transform.basis_xform_inv(lateral_vel) * 2, Color(1,0.5,0.5), 3) #pink
+	#draw_line(Vector2.ZERO, global_transform.basis_xform_inv(medial_vel) * 2, Color(0.5,0.5,1), 3) #light blue
+	#draw_line(Vector2.ZERO, global_transform.basis_xform_inv(lateral_vel) * 2, Color(1,0.5,0.5), 3) #pink
+	#draw_line(Vector2.ZERO + Vector2.UP * 20, global_transform.basis_xform_inv(sign(rot_dir) * Vector2.RIGHT.rotated(rotation)) * 52, Color(0,0,1), 3) #blue
 
 
 	# NOTES
@@ -99,14 +100,16 @@ func torque_calculation():
 #################################################################################################
 func player_input(delta):
 	# Gather Inputs: Forward/Backward
-	var fwd_dir = Vector2(0,0)
-	var bwd_dir = Vector2(0,0)
+	var fwd_dir = Vector2.UP.rotated(rotation)
+	
+	var fwd_input = Vector2(0,0)
+	var bwd_input = Vector2(0,0)
 	if Input.is_action_pressed(input_up):
-		fwd_dir = Vector2(0, -1)
+		fwd_input = Vector2(0, -1)
 	if Input.is_action_pressed(input_down):
-		bwd_dir = Vector2(0, 1)
+		bwd_input = Vector2(0, 1)
 	# Store Acceleration direction
-	accel_dir = (fwd_dir + bwd_dir).rotated(rotation).normalized()
+	accel_dir = (fwd_input + bwd_input).rotated(rotation).normalized()
 
 	# Gather Inputs: Left/Right
 	turn_left = Vector2(0,0)
@@ -125,11 +128,19 @@ func player_input(delta):
 	else:
 		r = 0
 
-
+	# if turn_left == Vector2.ZERO and turn_right == Vector2.ZERO:
+	# 	steer_dir = fwd_dir
+	# else:
+	# 	var reverse_blend = clamp(fwd_dir.dot(velocity.normalized()), -1.0, 1.0) # -1 = full reverse, 1 = full forward
+	# 	var forward_steer = (turn_left + turn_right).normalized()
+	# 	var backward_steer = -forward_steer
+	# 	steer_dir = forward_steer.lerp(backward_steer, clamp(-reverse_blend, 0.0, 1.0))
 	# Store steering inputs in a normalized vector
-	steer_dir = (turn_left + turn_right).normalized()
-	if steer_dir == Vector2(0,0):							# Find a way to put this into the equation and get rid of if statement
-		steer_dir = Vector2.UP.rotated(rotation)
+	var reverse_correct = sign(fwd_dir.dot(velocity))
+
+	steer_dir = (turn_left + turn_right).normalized() * reverse_correct
+	if steer_dir == Vector2(0,0):							
+		steer_dir = fwd_dir
 
 #################################################################################################
 func acceleration(delta):
@@ -201,48 +212,47 @@ func steering(delta):
 
 	var medial_friction = 0.9999 # WIP
 	velocity = (lateral_vel) + (medial_vel * medial_friction) + force_addition
-	##################################################################################################
-	############### Start Rotation ##############
-
-	# ALIGNING ROTATION NEEDS TO BE SMALL
-	# alignment = the direction the car wants to align to 
-	# alignment = velocity.normalized().dot(fwd_dir) # -1 to 1
-	# print_debug("alignment: ",	alignment)
-	# var misalignment = velocity.normalized().cross(fwd_dir)
-	# print_debug("misalignment:	", misalignment)
-	# var traction = clamp(alignment, 0.0, 1.0)
-	# print_debug("traction:	", traction)
-	# var aligning_force = traction * -misalignment
-	# print_debug("aligning_force: ",	aligning_force)
-	# END ALIGNING
-
-	var rot_dir = -sign(steer_degree)
-
-
-	rot_momentum +=  4 * rot_dir * delta
-
-
-	# TO DO NEXT
-	# Take the abs value of the alignment since it doesn't matter if it aligned fwd or bwd
+	#############################################################################
+	# ROTATION TO DO LIST #######################################################
+	
 	# rotational momentum can't really exist unless the car is sliding
 		# So, either use states, or replicate states doing some turn rate to velocity ratio to
 			# dictate if rot_momentum is used or just a constant turn radius is used.
-	# GPT ####################################################################
+		# ALSO: Rot_momentum needs two things (probably 2)
+			# Greater angular_friction at low velocity AND/OR greater angular_friction at Low rot_momentum
+	# Okay, I think rot_torque/speed should be low high low from 0 to 90 degrees, determined by velocity alignment to fwd, 45, perpindicular
+		# Also relative to velocity, but angular friction might be able to handle that part
+	#############################################################################
+	# Angular Friction ##########################################################
 	var speed_ratio = clamp(velocity.length() / 200, 0.0, 1.0)
-	var alignment = velocity.normalized().dot(fwd_dir) # 0 to 1
+	var alignment = abs(velocity.normalized().dot(fwd_dir)) # 0 to 1
 
 
 	# Less friction at higher speeds
-	var angular_friction = lerp(0.95, 0.99, speed_ratio * (1.0 - alignment))
-	############################################################################
-	print_debug("alignment: 	", alignment)
-	print_debug("weight:	", speed_ratio * (1.0 - alignment))
-	print_debug("angular_friction:	", angular_friction)
+	# Consider using 0.1 at the lerp min
+		# More angular friction at low speed - May need to change but certainly works as a bandaid
+	var angular_friction = lerp(0.1, 0.99, speed_ratio * (1.0 - alignment))
+	#############################################################################
+	# Alignment Force ###########################################################
+	var misalignment = vel_dir.cross(fwd_dir)
+	print_debug("misalignment:	", misalignment)
+	var traction = clamp(alignment, 0.0, 1.0)
+	print_debug("traction:	", traction)
+	var aligning_force = traction * -misalignment
+	print_debug("aligning_force: ", aligning_force)
+	#############################################################################
+	# Rotation Momentum #########################################################
+	rot_dir = -sign(steer_degree)
 
+	# Replace the 4 with a rot_speed or rot torque. Consider variability vs constant
+	# May want rot_speed to be a value that increases with velocity? Or torque or sudden change in velocity?
+	rot_momentum += (aligning_force + 4 * rot_dir) * delta
 	rot_momentum *= pow(angular_friction, delta)
-	rot_momentum = clamp(rot_momentum, -2, 2)
-	#print_debug("rot_momentum:	", rot_momentum)
-
+	rot_momentum = clamp(rot_momentum, -3, 3)
+	# print_debug("alignment: 	", alignment)
+	# print_debug("weight:	", speed_ratio * (1.0 - alignment))
+	# print_debug("angular_friction:	", angular_friction)
+	#############################################################################
 	rotation += rot_momentum * delta
 
 	# Working Code
