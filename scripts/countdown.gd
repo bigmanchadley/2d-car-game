0769cde53d8ea3e1c953921250cadb79fb0386eb
bigmanchadley <bigmanchadley@gmail.com>
2025-04-23 16:19:47 -0400

    Countdown done. No movement prevention yet

--- scripts/countdown.gd
+++ scripts/countdown.gd
@@ -1,11 +1,24 @@
 extends Node
 
 
+@onready var countdown_label = $Label
+
+const total_countdown_time = 3.9
+var time_left: float
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	time_left = total_countdown_time
+	countdown_label.text = str(total_countdown_time)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	time_left -= delta
+	if time_left > 1:
+		countdown_label.text = str(int(time_left))
+	else:
+		countdown_label.text = "GO!"
+		countdown_label.modulate.a -= delta
+
+	if countdown_label.modulate.a < 0:
+		queue_free()
